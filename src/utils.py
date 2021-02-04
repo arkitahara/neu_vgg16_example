5db97db7b3b982e8d5ab08e1ef01fb2fa0fcb369
Andrew R. Kitahara <akitahar@andrew.cmu.edu>
2021-02-03 21:40:59 -0500

    addding src code and added natsort to yml

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import os
+import glob
+from natsort import natsorted
+
+def get_files(directory, extension='*.png'):
+    """Fetch list of files with a given extension.
+    Args:
+        directory (string): path to directory to retrieve files
+        extension (string): file extension of files to retrieve
+
+    Returns:
+        files (list): sorted list of files retrieved
+    """
+    
+    files = glob.glob(os.path.join(directory, extension))
+    return sorted(files)
